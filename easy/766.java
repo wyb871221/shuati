759e1f676a0d79957fbd155b16c9ea4a87868865
yiwang <yiwang@factset.com>
2018-02-14 15:20:48 -0500

    adde solution for 566

--- easy/766.java
+++ easy/766.java
@@ -55,3 +55,15 @@ class Solution {
 }
 
 // time complexity = O(n) n is the number of nodes in the matrix, because you need to traverse every node once
+
+// simple solution
+class Solution {
+    public boolean isToeplitzMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length - 1; i++) {
+            for (int j = 0; j < matrix[i].length - 1; j++) {
+                if (matrix[i][j] != matrix[i + 1][j + 1]) return false;
+            }
+        }
+        return true;
+    }
+}
