c0149e30eaeb82e4aebafae1775aaf7e68983ef4
yiwang <yiwang@factset.com>
2018-02-16 16:05:07 -0500

    added solution for 566.java & 442.java, also added notes in README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # shuati
+
+Array category algorithm takeaways:
+
+1. If there's a range for the defined numbers in the array, create an array with numbers of the range
+eg. range given to you is integer from 0 to 10000, create an array with 10000 elements, this way you can achieve sorting in O(n) time
+eg 561.java
+
+2. If the range given by the problem is positive integer, then you can achieve O(1) space by using negative integer as a visited mark.
+eg. 442.java
--- easy/566.java
+++ easy/566.java
@@ -33,6 +33,7 @@ The height and width of the given matrix is in range [1, 100].
 The given r and c are all positive.
 */
 
+// my solution
 class Solution {
     public int[][] matrixReshape(int[][] nums, int r, int c) {
     	int row_num = nums.length;
@@ -57,4 +58,16 @@ class Solution {
     	}
     	return res;
     }
+}
+
+// concise solution
+class Solution {
+	public int[][] matrixReshape(int[][] nums, int r, int c) {
+	    int n = nums.length, m = nums[0].length;
+	    if (r*c != n*m) return nums;
+	    int[][] res = new int[r][c];
+	    for (int i=0;i<r*c;i++) 
+	        res[i/c][i%c] = nums[i/m][i%m];
+	    return res;
+	}
 }
