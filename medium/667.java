0bcdc19fc7fa470bec4192c405e53aaf824d3e4c
ywang_mac_air <kevinwong19871221@gmail.com>
2018-03-20 23:17:32 -0400

    added solution for 667

---
+++ medium/667.java
@@ -0,0 +1,42 @@
+/*
+667. Beautiful Arrangement II
+
+Given two integers n and k, you need to construct a list which contains n different positive integers ranging from 1 to n and obeys the following requirement: 
+Suppose this list is [a1, a2, a3, ... , an], then the list [|a1 - a2|, |a2 - a3|, |a3 - a4|, ... , |an-1 - an|] has exactly k distinct integers.
+
+If there are multiple answers, print any of them.
+
+Example 1:
+Input: n = 3, k = 1
+Output: [1, 2, 3]
+Explanation: The [1, 2, 3] has three different positive integers ranging from 1 to 3, and the [1, 1] has exactly 1 distinct integer: 1.
+Example 2:
+Input: n = 3, k = 2
+Output: [1, 3, 2]
+Explanation: The [1, 3, 2] has three different positive integers ranging from 1 to 3, and the [2, 1] has exactly 2 distinct integers: 1 and 2.
+Note:
+The n and k are in the range 1 <= k < n <= 104.
+*/
+
+class Solution {
+    public int[] constructArray(int n, int k) {
+        int[] ret = new int[n];
+        int count1 = 1;
+        int count2 = k + 1;
+        
+        for (int i = 0; i < k; i += 2) {
+            ret[i] = count1;
+            ret[i + 1] = count2;
+            count1++;
+            count2--;
+        }
+        
+        if (ret[k] == 0) ret[k] = count1; 
+        
+        for (int i = k + 1; i < n; ++i) {
+            ret[i] = i + 1;
+        }
+        
+        return ret;
+    }
+}
