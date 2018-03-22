e425078d8f44fb616ca566db9eec8153f9277ad7
ywang_mac_air <kevinwong19871221@gmail.com>
2018-03-22 08:57:50 -0400

    added solution for 238.java

---
+++ medium/238.java
@@ -0,0 +1,47 @@
+/*
+238. Product of Array Except Self
+
+Given an array of n integers where n > 1, nums, return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Solve it without division and in O(n).
+
+For example, given [1,2,3,4], return [24,12,8,6].
+
+Follow up:
+Could you solve it with constant space complexity? (Note: The output array does not count as extra space for the purpose of space complexity analysis.)
+
+Given numbers [2, 3, 4, 5], regarding the third number 4, the product of array except 4 is 2*3*5 which consists of two parts: left 2*3 and right 5. The product is left*right. We can get lefts and rights:
+
+Numbers:     2    3    4     5
+Lefts:            2  2*3 2*3*4
+Rights:  3*4*5  4*5    5      
+Let’s fill the empty with 1:
+
+Numbers:     2    3    4     5
+Lefts:       1    2  2*3 2*3*4
+Rights:  3*4*5  4*5    5     1
+We can calculate lefts and rights in 2 loops. The time complexity is O(n).
+
+We store lefts in result array. If we allocate a new array for rights. The space complexity is O(n). To make it O(1), we just need to store it in a variable which is 
+*/
+
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] res = new int[n];
+                
+        int left = 1;
+        res[0] = 1;
+        for (int i = 1; i < n; ++i) {
+            left = left * nums[i - 1];
+            res[i] = left;
+        }
+        
+        int right = 1;
+        for (int i = n - 1; i > 0; --i) {
+            right = right * nums[i];
+            res[i - 1] = right * res[i - 1];
+        }
+        return res;
+    }
+}
