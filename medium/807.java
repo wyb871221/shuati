4aed61f6c6b98489fbfcdc966e29c96b708895d2
ywang_mac_air <kevinwong19871221@gmail.com>
2018-03-28 00:23:27 -0400

    added concise solution for 807

--- medium/807.java
+++ medium/807.java
@@ -29,25 +29,17 @@ gridNew = [ [8, 4, 8, 7],
 
 */
 
+// O(n^2)
 class Solution {
     public int maxIncreaseKeepingSkyline(int[][] grid) {
         int[] top = new int[grid[0].length];
         int[] left = new int[grid.length];
         
         for (int i = 0; i < grid.length; ++i) {
-            int max = 0;
             for (int j = 0; j < grid[0].length; ++j) {
-                max = Math.max(max, grid[i][j]);
+                left[i] = Math.max(left[i], grid[i][j]);
+                top[j] = Math.max(top[j], grid[i][j]);
             }
-            left[i] = max;
-        }
-        
-        for (int i = 0; i < grid[0].length; ++i) {
-            int max = 0;
-            for (int j = 0; j < grid.length; ++j) {
-                max = Math.max(max, grid[j][i]);
-            }
-            top[i] = max;
         }
         
         int res = 0;
