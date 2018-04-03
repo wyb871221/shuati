ad90348953c8fbd6ba80317b2ba69229be8e2043
ywang_mac_air <kevinwong19871221@gmail.com>
2018-04-02 21:55:01 -0400

    266,412,544

---
+++ easy/266.java
@@ -0,0 +1,28 @@
+/*
+266. Palindrome Permutation
+
+Given a string, determine if a permutation of the string could form a palindrome.
+
+For example,
+"code" -> False, "aab" -> True, "carerac" -> True.
+
+*/
+
+class Solution {
+    public boolean canPermutePalindrome(String s) {
+        int[] map = new int[128];
+        int count = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            char c = s.charAt(i);
+            if (map[c] == 0) {
+                map[c] = 1;
+                count++;
+            } else {
+                map[c] = 0;
+                count--;
+            }
+        }
+        
+        return count <= 1;
+    }
+}
